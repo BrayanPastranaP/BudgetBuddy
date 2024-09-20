b555bb9bed3ad7472c1d5540621044a784dfecf1
Brayan Pastrana <brayan@gmail.com>
2024-09-20 10:26:49 -0600

    login

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require 'config/functions.php';
 
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+  header("Location: config/logout.php");
+  exit();
+}
+
 // Obtener todos los ingresos
 $sql_ingresos = "SELECT SUM(monto) as total_ingreso FROM ingresos";
 $result_ingresos = $conn->query($sql_ingresos);
@@ -17,118 +24,109 @@ $total_gasto = $result_gastos->fetch_assoc()['total_gasto'] ?? 0;
 
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>BudgetBuddy Dashboard</title>
-    <link rel="stylesheet" href="styles.css" />
 
-    <!--==================== UNICONS ====================-->
-    <link
-      rel="stylesheet"
-      href="https://unicons.iconscout.com/release/v3.0.6/css/line.css"
-    />
-  </head>
-  <body>
-    <div class="container">
-      <!-- Sidebar -->
-      <!-- Botón del menú hamburguesa -->
-      <div class="hamburger-menu">
-        <i class="uil uil-bars"></i>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>BudgetBuddy Dashboard</title>
+  <link rel="stylesheet" href="styles.css" />
+
+  <!--==================== UNICONS ====================-->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css" />
+</head>
+
+<body>
+  <div class="container">
+    <!-- Sidebar -->
+    <!-- Botón del menú hamburguesa -->
+    <div class="hamburger-menu">
+      <i class="uil uil-bars"></i>
+    </div>
+
+    <aside class="sidebar">
+      <div class="container__nav">
+        <div class="logo">
+          <img src="img/logo.png" alt="Logotipo" />
+          <h2>BudgetBuddy</h2>
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="index.php" class="nav__active"><i class="uil uil-estate"></i> Menú principal</a>
+            </li>
+            <li>
+              <a href="ingresos.php"><i class="uil uil-plus-circle"></i> Ingresos</a>
+            </li>
+            <li>
+              <a href="gastos.php"><i class="uil uil-minus-circle"></i> Gastos</a>
+            </li>
+            <li>
+              <a href="movimientos.php"><i class="uil uil-minus-circle"></i> Movimientos</a>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <aside class="sidebar">
-        <div class="container__nav">
-          <div class="logo">
-            <img src="img/logo.png" alt="Logotipo" />
-            <h2>BudgetBuddy</h2>
+      <div class="sidebar-footer">
+        <a href="#"><i class="uil uil-setting"></i> Configuración</a>
+        <a href="config/logout.php"><i class="uil uil-sign-out-alt"></i> Cerrar sesión</a>
+      </div>
+
+    </aside>
+
+    <!-- Main Content -->
+    <main class="main-content">
+      <header>
+        <h1>¡Bienvenido de nuevo!</h1>
+        <h2>Pietro Filiberto</h2>
+      </header>
+
+      <div class="content-flex">
+        <!-- Stats Section -->
+        <section class="stats">
+          <div class="stat-item chart banner_inicial">
+            <h3>Recomendaciones</h3>
+            <br>
+
+            <p>
+              <?php echo evaluarFinanzas(); ?>
+            </p>
           </div>
+          <div class="stat-item">
+            <h3>Ingresos semanales</h3>
+            <p class="degradado total_ingreso">$<?php echo $total_ingreso; ?></p>
+          </div>
+          <div class="stat-item">
+            <h3>Gasto semanal</h3>
+            <p class="degradado total_gasto">$<?php echo $total_gasto; ?></p>
+          </div>
+          <div class="stat-item chart">
+            <h3>Veamos cómo van tus finanzas</h3>
+            <!-- Contenedor para la gráfica -->
+            <canvas id="myChart" width="400" height="200"></canvas>
+          </div>
+        </section>
 
-          <nav>
-            <ul>
-              <li>
-                <a href="index.php" class="nav__active"
-                  ><i class="uil uil-estate"></i> Menú principal</a
-                >
-              </li>
-              <li>
-                <a href="ingresos.php"
-                  ><i class="uil uil-plus-circle"></i> Ingresos</a
-                >
-              </li>
-              <li>
-                <a href="gastos.php"
-                  ><i class="uil uil-minus-circle"></i> Gastos</a
-                >
-              </li>
-              <li>
-                <a href="movimientos.php"
-                  ><i class="uil uil-minus-circle"></i> Movimientos</a
-                >
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <!-- Ads Section -->
+        <section class="ads">
+          <div class="ad">
+            <img src="img/add.png" alt="Anuncio 1" />
+          </div>
+          <div class="stat-item comparacion_mensual">
+            <h3>Comparación mensual</h3>
+            <p>
+              Has gastado <br /><span class="degradado">$1,200</span><br />
+              más que el mes pasado
+            </p>
+            <button>Generar reporte</button>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="app.js"></script>
+</body>
 
-        <div class="sidebar-footer">
-          <a href="#"><i class="uil uil-setting"></i> Configuración</a>
-          <a href="login.php"
-            ><i class="uil uil-sign-out-alt"></i> Cerrar sesión</a
-          >
-        </div>
-      </aside>
-
-      <!-- Main Content -->
-      <main class="main-content">
-        <header>
-          <h1>¡Bienvenido de nuevo!</h1>
-          <h2>Pietro Filiberto</h2>
-        </header>
-
-        <div class="content-flex">
-          <!-- Stats Section -->
-          <section class="stats">
-            <div class="stat-item chart banner_inicial">
-              <h3>Recomendaciones</h3>
-              <br>
-              
-              <p>
-                <?php echo evaluarFinanzas();?>
-              </p>
-            </div>
-            <div class="stat-item">
-              <h3>Ingresos semanales</h3>
-              <p class="degradado total_ingreso">$<?php echo $total_ingreso; ?></p>
-            </div>
-            <div class="stat-item">
-              <h3>Gasto semanal</h3>
-              <p class="degradado total_gasto">$<?php echo $total_gasto; ?></p>
-            </div>
-            <div class="stat-item chart">
-              <h3>Veamos cómo van tus finanzas</h3>
-              <!-- Contenedor para la gráfica -->
-              <canvas id="myChart" width="400" height="200"></canvas>
-            </div>
-          </section>
-
-          <!-- Ads Section -->
-          <section class="ads">
-            <div class="ad">
-              <img src="img/add.png" alt="Anuncio 1" />
-            </div>
-            <div class="stat-item comparacion_mensual">
-              <h3>Comparación mensual</h3>
-              <p>
-                Has gastado <br /><span class="degradado">$1,200</span><br />
-                más que el mes pasado
-              </p>
-              <button>Generar reporte</button>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="app.js"></script>
-  </body>
 </html>
